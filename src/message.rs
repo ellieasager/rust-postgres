626b8ab4f703bd0d0e25e936b260d69d5a6cf3ff
Ellie A Sager <110849420+EllieSager-Forte@users.noreply.github.com>
2024-07-27 13:46:55 -0700

    Yay! proper error handling!

--- src/message.rs
+++ src/message.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, Responder};
+use actix_web::{web, Responder, ResponseError};
 use serde::ser::SerializeStruct;
 use serde::Deserialize;
 use serde::{Serialize, Serializer};
@@ -32,6 +32,16 @@ pub struct CreateRequest {
     pub content: String,
 }
 
+#[derive(Debug)]
+pub struct MyError(String);
+
+impl std::fmt::Display for MyError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+impl ResponseError for MyError {}
+
 #[derive(Serialize)]
 pub struct ListResponse {
     pub messages: Vec<Message>,
@@ -44,36 +54,49 @@ pub async fn create_message(
     println!("creating a message");
 
     let id = sqlx::types::Uuid::from_u128(uuid::Uuid::new_v4().as_u128());
-    let row: (sqlx::types::Uuid,) =
-        sqlx::query_as("insert into messages (id, content) values ($1, $2) returning id")
-            .bind(id.to_owned())
-            .bind(req.content.to_owned())
-            .fetch_one(&data.db_pool)
-            .await
-            .expect("postgres insertion error");
+    let response: Result<(sqlx::types::Uuid,), sqlx::Error> =
+    sqlx::query_as("insert into messages (id, content) values ($1, $2) returning id")
+        .bind(id.to_owned())
+        .bind(req.content.to_owned())
+        .fetch_one(&data.db_pool)
+        .await;
 
-    println!("row INSERTED: {:?}", row);
-    let message = Message {
-        id,
-        content: req.content.to_owned(),
-    };
-    println!("created a message");
-
-    web::Json(message)
+    match response {
+        Ok(row) => {
+            println!("row INSERTED: {:?}", row);
+            let message = Message {
+                id,
+                content: req.content.to_owned(),
+            };
+            Ok(web::Json(message))
+        }, 
+        Err(sqlx_error) => {
+            println!(
+                "\n\n=== error creating a message \n{:?}",
+                sqlx_error.to_string());
+            Err(MyError(sqlx_error.to_string()))
+        },
+    }
 }
 
 pub async fn list_messages(data: web::Data<AppState>) -> impl Responder {
     println!("listing messages:");
 
     let select_query = sqlx::query_as::<_, Message>("SELECT id, content FROM messages");
-    let messages: Vec<Message> = select_query
-        .fetch_all(&data.db_pool)
-        .await
-        .expect("postgres selection error");
-    println!(
-        "\n\n=== select messages with query.map...: \n{:?}",
-        messages
-    );
+    let response = select_query.fetch_all(&data.db_pool).await;
 
-    web::Json(ListResponse { messages })
+    match response {
+        Ok(messages) => {
+            println!(
+                "\n\n=== select messages: \n{:?}",
+                messages);
+            Ok(web::Json(ListResponse { messages }))
+        },
+        Err(sqlx_error) => {
+            println!(
+                "\n\n=== error selecting messages: \n{:?}",
+                sqlx_error.to_string());
+            Err(MyError(sqlx_error.to_string()))
+        },
+    }
 }
